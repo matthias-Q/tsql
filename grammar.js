a38d51272fc6f7514720f4b0f6d1c3f8c493bb4d
Matthias Queitsch <matthias.queitsch@mailbox.org>
2023-02-17 18:25:56 +0100

    setup branch 1

--- grammar.js
+++ grammar.js
@@ -37,6 +37,7 @@ module.exports = grammar({
       ),
     ),
 
+    keyword_select1: _ => make_keyword("select1"),
     keyword_select: _ => make_keyword("select"),
     keyword_delete: _ => make_keyword("delete"),
     keyword_insert: _ => make_keyword("insert"),
